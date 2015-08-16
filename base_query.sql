660b3699ca7ff0b20c4fc7a63e4c28ad3664b4fc
waymanglover <waymanglover@gmail.com>
2015-08-16 16:03:12 -0400

    Initial commit.

---
+++ base_query.sql
@@ -0,0 +1,18 @@
+select cards.cardname,
+cards.playerclass,
+case 
+	when deck_lists.cardname is null then 0 
+	else count(*)
+end as [Total],
+case 
+when deck_lists.cardname is null then 0.0
+else count(*)/355.0 * 100.0
+end as [Percent],
+avg(coalesce(deck_lists.amount, 0)) as [Average Per Deck]
+from cards
+left join deck_lists
+on cards.cardname = deck_lists.cardname
+where cards.cardset in ('Classic', 'Goblins vs Gnomes')
+and cards.playerclass <> 'Neutral'
+group by cards.cardname
+order by Percent desc
